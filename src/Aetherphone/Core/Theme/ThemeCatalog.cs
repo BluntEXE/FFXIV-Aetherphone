e6c1ae0ba4e32ca88f9ab16a74581816196d0d44
XeldarAlz <xeldaralz@hotmail.com>
2026-08-01 01:33:26 +0300

    feat(cases): replace the art case lineup with 18 new hand-painted cases
    Retire Ironworks, Emberforge, Voidsent, Bulwark, Carbonweave and
    Alabaster. Add Foxtail, Frostnip, Nacrelle, Verdigry, Voltpup,
    Aetherite, Chocorunner, Gembuncle, Ironsworn, Kuponuff, Prickletot,
    Amourette, Barkleigh, Emberlash, Tidecaller, Vesperine and Whiskerlune,
    and redraw Silkie in pink. Fallback tints are sampled from each case's
    metal band. Anyone on a retired case falls back to Titanium.

--- src/Aetherphone/Core/Theme/ThemeCatalog.cs
+++ src/Aetherphone/Core/Theme/ThemeCatalog.cs
@@ -16,13 +16,24 @@ internal static class ThemeCatalog
     private static readonly PhoneCase[] BuiltInCases =
     {
         PhoneCase.Color(DefaultCaseName, new Vector4(0.145f, 0.145f, 0.170f, 1f)),
-        PhoneCase.Art("Ironworks", new Vector4(0.335f, 0.375f, 0.421f, 1f)),
-        PhoneCase.Art("Emberforge", new Vector4(0.473f, 0.259f, 0.088f, 1f)),
-        PhoneCase.Art("Voidsent", new Vector4(0.190f, 0.122f, 0.361f, 1f)),
-        PhoneCase.Art("Bulwark", new Vector4(0.224f, 0.249f, 0.277f, 1f)),
-        PhoneCase.Art("Carbonweave", new Vector4(0.122f, 0.133f, 0.153f, 1f)),
-        PhoneCase.Art("Alabaster", new Vector4(0.826f, 0.798f, 0.751f, 1f)),
-        PhoneCase.Art("Silkie", new Vector4(0.918f, 0.894f, 0.867f, 1f)),
+        PhoneCase.Art("Foxtail", new Vector4(0.310f, 0.196f, 0.114f, 1f)),
+        PhoneCase.Art("Frostnip", new Vector4(0.599f, 0.698f, 0.746f, 1f)),
+        PhoneCase.Art("Nacrelle", new Vector4(0.562f, 0.473f, 0.329f, 1f)),
+        PhoneCase.Art("Verdigry", new Vector4(0.490f, 0.465f, 0.360f, 1f)),
+        PhoneCase.Art("Voltpup", new Vector4(0.158f, 0.279f, 0.324f, 1f)),
+        PhoneCase.Art("Aetherite", new Vector4(0.624f, 0.672f, 0.907f, 1f)),
+        PhoneCase.Art("Chocorunner", new Vector4(0.717f, 0.541f, 0.186f, 1f)),
+        PhoneCase.Art("Gembuncle", new Vector4(0.685f, 0.694f, 0.436f, 1f)),
+        PhoneCase.Art("Ironsworn", new Vector4(0.273f, 0.311f, 0.371f, 1f)),
+        PhoneCase.Art("Kuponuff", new Vector4(0.780f, 0.666f, 0.587f, 1f)),
+        PhoneCase.Art("Prickletot", new Vector4(0.570f, 0.635f, 0.380f, 1f)),
+        PhoneCase.Art("Amourette", new Vector4(0.969f, 0.877f, 0.860f, 1f)),
+        PhoneCase.Art("Barkleigh", new Vector4(0.744f, 0.549f, 0.260f, 1f)),
+        PhoneCase.Art("Emberlash", new Vector4(0.794f, 0.697f, 0.490f, 1f)),
+        PhoneCase.Art("Tidecaller", new Vector4(0.266f, 0.516f, 0.556f, 1f)),
+        PhoneCase.Art("Vesperine", new Vector4(0.719f, 0.690f, 0.949f, 1f)),
+        PhoneCase.Art("Whiskerlune", new Vector4(0.242f, 0.222f, 0.360f, 1f)),
+        PhoneCase.Art("Silkie", new Vector4(1.000f, 0.922f, 0.918f, 1f)),
     };
 
     public static IReadOnlyList<PhoneCase> Cases { get; } = BuiltInCases;
