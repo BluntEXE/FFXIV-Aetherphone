29f10ebf696cf7e4630bf08bab012c9f5e81c2d1
Syrilai <30000694+Syrilai@users.noreply.github.com>
2026-08-05 11:27:26 +0200

    Update primary and alias command in debug

--- src/Aetherphone/Core/AepConstants.cs
+++ src/Aetherphone/Core/AepConstants.cs
@@ -4,11 +4,13 @@ internal static class AepConstants
 {
     #if DEBUG
     public const string Name = "AetherphoneDev";
+    public const string PrimaryCommand = "/phonedev";
+    public const string AliasCommand = "/aetherphonedev";
     #else
     public const string Name = "Aetherphone";
-    #endif
     public const string PrimaryCommand = "/phone";
     public const string AliasCommand = "/aetherphone";
+    #endif
     public const string DiscordUrl = "https://discord.gg/3HbJCscMyS";
     public const string WebsiteUrl = "https://www.aetherphone.net";
     public const string PatreonUrl = "https://www.patreon.com/XeldarAlz";
